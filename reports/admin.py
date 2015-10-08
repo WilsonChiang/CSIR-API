b3cb213cd31789d5ac9cc5f86dc794b327f4a761
falahan <amuise3@hotmail.ca>
2015-10-08 15:30:43 +0000

    ISSUE-2: Added Sections, Maps to Admin

--- reports/admin.py
+++ reports/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from reports.models import CountryReport
+from reports.models import Maps
+from reports.models import Section
 
 class CountryReportAdmin(admin.ModelAdmin):
     model = CountryReport
     
-    
+class MapsAdmin(admin.ModelAdmin):
+    model = Maps
+
+class SectionAdmin(admin.ModelAdmin):
+    model = Section
+
 admin.site.register(CountryReport)
+admin.site.register(Maps)
+admin.site.register(Section)
